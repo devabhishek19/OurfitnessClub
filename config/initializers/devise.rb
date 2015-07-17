d1dbedd857055628a8aad9a5a8e9d5695410f47f
Abhishek Patel <dev.abhishek19@gmail.com>
2015-07-16 23:39:55 -0400

    changes for the image load in heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.secret_key = '54c878a5bbb129f7a7d13ed3c83baae29132aa2d46ba77497eb48413d332168dce7b88b6cffde848421de14e2674f11dbe1826d60122f80000ce2842908d73e9'
 end
